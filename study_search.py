84f2ea7263be38693fbaeb1a9cbc5e6c552d16ca
jwaterford <jwaterford@ucsd.edu>
2023-08-03 15:59:00 -0700

    updates to search and requests module

--- study_search.py
+++ study_search.py
@@ -7,3 +7,37 @@ import json
 correct_answer = json.load(open("Apyterx.json"))
 
 print("There should be {x} studies in the response".format(x=len(correct_answer['matched_studies'])))
+
+
+
+import requests
+import json
+
+payload = {'property':'ot:ottTaxonName', 'value':'Apteryx australis', 'verbose':'true'}
+headers =  {"Content-Type":"application/json"}
+r = requests.post('https://api.opentreeoflife.org/v3/studies/find_trees',  params=json.dumps(payload), headers=headers)
+data = r.json()
+## Try with OS / BASH CURL?
+
+#data = json.load(open("Aptyrex_trees.txt", r))
+#print(data)
+
+temp_list = []
+for i in data['matched_studies']:
+    for x in i:
+        x = i['ot:studyId']
+        if x not in temp_list:
+            temp_list.append(x)
+
+
+Ott_list = []
+for i in temp_list:
+    for word in i:
+        for letter in word:
+            if letter == 'o':
+                Ott_list.append(i)
+            else: break
+            
+        
+#print(Ott_list)
+print("There are  {x} studies in this list.".format(x = len(Ott_list)))
